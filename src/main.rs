257032d17bc073741500380ea71968cfa402c186
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-05-10 16:24:53 +0200

    parallelized version

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 extern crate petgraph;
 extern crate rand;
+extern crate rayon;
+
+use std::sync::{RwLock, Arc};
 
 use petgraph::prelude::*;
+use petgraph::graph::EdgeReference;
 use petgraph::algo::{DfsSpace, has_path_connecting, kosaraju_scc};
 use petgraph::dot::Dot;
+
 use rand::{SeedableRng, distributions::{Sample, Range}};
+
+use rayon::prelude::*;
 
 const N: usize = 20;
 const DELTA: usize = 1;
@@ -33,42 +40,62 @@ fn main() {
     // 2. If the arc connects two nodes not connected, take it.
     // 3. When all the graph is strongly connected, if there are still link available that can be used respecting DELTA, take them in order
 
-    let mut sorted_edges = Vec::from(logic.raw_edges());
-    sorted_edges.sort_unstable_by(|e1, e2| {
-        match e2.weight.partial_cmp(&e1.weight) {
-            Some(o) => o,
-            None => std::cmp::Ordering::Equal
-        }
-    });
-    //let mut connected = vec![false; N];
-    //let mut unconnected = N;
-    let mut indegree = vec![0;N];
-    let mut outdegree = vec![0;N];
+    let mut max_weight = RwLock::new((0, 0.0));
     let mut phisical = Graph::new();
     for i in 0..N {
         phisical.add_node(i);
     }
-    // Try to take the heaviest Hamilton Cycle
-    let mut workspace = DfsSpace::new(&phisical);
-    for e in &sorted_edges {
-        let a = e.source();
-        let b = e.target();
-        let w = e.weight;
-        if outdegree[a.index()] < DELTA && indegree[b.index()] < DELTA && 
-            !has_path_connecting(&phisical, a, b, Some(&mut workspace)) {
-                phisical.add_edge(a, b, w);
-                outdegree[a.index()] += 1;
-                indegree[b.index()] += 1;
+
+    (0usize..N).into_par_iter().for_each(|start| {
+        let mut tot_weight = 0.0;
+        let mut connected = vec![false; N];
+        let mut unconnected = N-1;
+        connected[start] = true;
+        let mut a = NodeIndex::new(start);
+        while unconnected > 0 {
+            let e:EdgeReference<_> = logic.edges_directed(a, Direction::Outgoing)
+                .filter(|e| !connected[e.target().index()])
+                .max_by(|e1, e2| {
+                    match e2.weight().partial_cmp(e1.weight()) {
+                        Some(o) => o,
+                        None => std::cmp::Ordering::Equal
+                    }
+                }).unwrap();
+            unconnected -= 1;
+            connected[e.target().index()] = true;
+            a = e.target();
+            tot_weight += e.weight();
         }
-    }
+        let e = logic.find_edge(a, NodeIndex::new(start)).unwrap();
+        tot_weight += logic.edge_weight(e).unwrap();
+        if max_weight.read().unwrap().1 < tot_weight {
+            *max_weight.write().unwrap() = (start, tot_weight);
+        }
+    });
 
-    // If there are more than one sccs, try to connect them
-    let sccs = kosaraju_scc(&phisical);
-    if sccs.len() > 1 {
-        
+    let start = max_weight.into_inner().unwrap().0;
+    let mut connected = vec![false; N];
+    let mut unconnected = N-1;
+    connected[start] = true;
+    let mut a = NodeIndex::new(start);
+    while unconnected > 0 {
+        let e:EdgeReference<_> = logic.edges_directed(a, Direction::Outgoing)
+            .filter(|e| !connected[e.target().index()])
+            .max_by(|e1, e2| {
+                match e2.weight().partial_cmp(e1.weight()) {
+                    Some(o) => o,
+                    None => std::cmp::Ordering::Equal
+                }
+            }).unwrap();
+        phisical.add_edge(a, e.target(), e.weight());
+        unconnected -= 1;
+        connected[e.target().index()] = true;
+        a = e.target();
     }
+    let e = logic.find_edge(a, NodeIndex::new(start)).unwrap();
+    phisical.add_edge(a, NodeIndex::new(start), logic.edge_weight(e).unwrap());
     
-
+    
     let dot = Dot::new(&phisical);
     println!("{:?}", dot);
 }
