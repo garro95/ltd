7583c218475bca81e199f19fa5e3aaffefaf3129
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2018-06-13 13:53:51 +0200

    Remove dead code

--- src/main.rs
+++ src/main.rs
@@ -23,24 +23,17 @@ use std::path::PathBuf;
 use std::process::{Command, Stdio};
 use std::ffi::OsString;
 use std::io::Write;
-use std::collections::{VecDeque, hash_map::{HashMap, Entry::{Occupied, Vacant}}, BinaryHeap};
-use std::hash::Hash;
 use std::cmp::Ordering;
 
 use petgraph::prelude::*;
 use petgraph::graph::EdgeReference;
 use petgraph::dot::Dot;
-use petgraph::algo::{Measure, DfsSpace, astar, has_path_connecting, toposort};
+use petgraph::algo::astar;
 use petgraph::visit::{
     EdgeRef,
-    GraphBase,
-    IntoEdges,
-    VisitMap,
-    Visitable,
 };
 
 use rand::{SeedableRng, XorShiftRng, distributions::{Sample, Range}};
-use rand::seq::sample_slice;
 
 use quicli::prelude::*;
 
@@ -359,154 +352,4 @@ fn output_result(g: &Graph<usize, f64>, p: PathBuf) {
         .spawn().expect("Failed to run graphviz' dot command");
     let mut stdin = graphviz.stdin.as_mut().expect("Failed to open graphviz' dot stdin");
     write!(&mut stdin, "{:?}", dot).unwrap();
-}
-
-fn max_astar<G, F, H, K, IsGoal>(graph: G, start: G::NodeId, mut is_goal: IsGoal,
-                                     mut edge_cost: F, mut estimate_cost: H)
-    -> Option<(K, Vec<G::NodeId>)>
-    where G: IntoEdges + Visitable,
-          IsGoal: FnMut(G::NodeId) -> bool,
-          G::NodeId: Eq + Hash,
-          F: FnMut(G::EdgeRef) -> K,
-          H: FnMut(G::NodeId) -> K,
-          K: Measure + Copy,
-{
-    let mut visited = graph.visit_map();
-    let mut visit_next = BinaryHeap::new();
-    let mut scores = HashMap::new();
-    let mut path_tracker = PathTracker::<G>::new();
-
-    let zero_score = K::default();
-    scores.insert(start, zero_score);
-    visit_next.push(MinScored(estimate_cost(start), start));
-
-    while let Some(MinScored(_, node)) = visit_next.pop() {
-        if is_goal(node) {
-            let path = path_tracker.reconstruct_path_to(node);
-            let cost = scores[&node];
-            return Some((cost, path));
-        }
-
-        // Don't visit the same node several times, as the first time it was visited it was using
-        // the shortest available path.
-        if !visited.visit(node) {
-            continue
-        }
-
-        // This lookup can be unwrapped without fear of panic since the node was necessarily scored
-        // before adding him to `visit_next`.
-        let node_score = scores[&node];
-
-        for edge in graph.edges(node) {
-            let next = edge.target();
-            if visited.is_visited(&next) {
-                continue
-            }
-
-            let mut next_score = if node_score > edge_cost(edge) {edge_cost(edge)} else {node_score};
-
-            match scores.entry(next) {
-                Occupied(ent) => {
-                    let old_score = *ent.get();
-                    if next_score < old_score {
-                        *ent.into_mut() = next_score;
-                        path_tracker.set_predecessor(next, node);
-                    } else {
-                        next_score = old_score;
-                    }
-                },
-                Vacant(ent) => {
-                    ent.insert(next_score);
-                    path_tracker.set_predecessor(next, node);
-                }
-            }
-
-            let next_estimate_score = if next_score > estimate_cost(next) {estimate_cost(next)} else {next_score};
-            visit_next.push(MinScored(next_estimate_score, next));
-        }
-    }
-
-    None
-}
-
-
-
-
-struct PathTracker<G>
-    where G: GraphBase,
-          G::NodeId: Eq + Hash,
-{
-    came_from: HashMap<G::NodeId, G::NodeId>,
-}
-
-impl<G> PathTracker<G>
-    where G: GraphBase,
-          G::NodeId: Eq + Hash,
-{
-    fn new() -> PathTracker<G> {
-        PathTracker {
-            came_from: HashMap::new(),
-        }
-    }
-
-    fn set_predecessor(&mut self, node: G::NodeId, previous: G::NodeId) {
-        self.came_from.insert(node, previous);
-    }
-
-    fn reconstruct_path_to(&self, last: G::NodeId) -> Vec<G::NodeId> {
-        let mut path = vec![last];
-
-        let mut current = last;
-        while let Some(&previous) = self.came_from.get(&current) {
-            path.push(previous);
-            current = previous;
-        }
-
-        path.reverse();
-
-        path
-    }
-}
-
-
-#[derive(Copy, Clone, Debug)]
-pub struct MinScored<K, T>(pub K, pub T);
-
-impl<K: PartialOrd, T> PartialEq for MinScored<K, T> {
-    #[inline]
-    fn eq(&self, other: &MinScored<K, T>) -> bool {
-        self.cmp(other) == Ordering::Equal
-    }
-}
-
-impl<K: PartialOrd, T> Eq for MinScored<K, T> {}
-
-impl<K: PartialOrd, T> PartialOrd for MinScored<K, T> {
-    #[inline]
-    fn partial_cmp(&self, other: &MinScored<K, T>) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<K: PartialOrd, T> Ord for MinScored<K, T> {
-    #[inline]
-    fn cmp(&self, other: &MinScored<K, T>) -> Ordering {
-        let a = &self.0;
-        let b = &other.0;
-        if a == b {
-            Ordering::Equal
-        } else if a < b {
-            Ordering::Greater
-        } else if a > b {
-            Ordering::Less
-        } else if a != a && b != b {
-            // these are the NaN cases
-            Ordering::Equal
-        } else if a != a {
-            // Order NaN less, so that it is last in the MinScore order
-            Ordering::Less
-        } else {
-            Ordering::Greater
-        }
-    }
 }
